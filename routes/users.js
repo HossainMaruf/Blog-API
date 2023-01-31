e9b53fba9be5f11aac1838ac5e9af141825e0896
HossainMaruf <maruficepustian@gmail.com>
2023-01-31 11:39:32 +0600

    user routes added

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,20 @@
 const router = require('express').Router();
 
+// get the model
+const User = require("../models/User");
+
+// get user by id
+router.get("/:userID", async function(req, res, next) {
+		try {
+			const user = await User.findById(req.params.userID);
+			if(user) {
+				return res.status(200).json(user);
+			} else {
+				return res.status(404).json("User not found");
+			}
+		} catch(error) {
+			return res.status(500).json("Something went wrong!");	
+		}
+})
 
 module.exports = router;
