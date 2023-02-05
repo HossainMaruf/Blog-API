cb75a642bdebf77455046b9945eeb3d2d77e2ac4
HossainMaruf <maruficepustian@gmail.com>
2023-02-05 22:40:01 +0600

    DB URL issue fixed

--- helpers/DB.js
+++ helpers/DB.js
@@ -4,7 +4,7 @@ const {MONGO_URL, LOCAL_MONGO_URL} = require('../config.js');
 class DB {
 	static ConnectDB() {
 		mongoose.connect(
-		  mongodb+srv://MarufHossain:maruf170626@portfolio-blog.hwgz8yk.mongodb.net/blog?retryWrites=true&w=majority,
+		  "mongodb+srv://MarufHossain:maruf170626@portfolio-blog.hwgz8yk.mongodb.net/blog?retryWrites=true&w=majority",
 		  { useNewUrlParser: true, useUnifiedTopology: true},
 		  () => {
 		    console.log("DB Connected");
