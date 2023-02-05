8a83b40623de4ac6096c97964c35705748ca1feb
HossainMaruf <maruficepustian@gmail.com>
2023-02-05 14:11:58 +0600

    Port and DB connection added

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.get('/api/skills', function(req, res) {
 	res.send(skills);
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, function() {
 	console.log(`Server is running on port ${PORT}`);		
 })
